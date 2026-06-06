a2e9b78882f6efa0231559289dd9e4fb04cc27ce
phamlam24 <lamphamthedev@gmail.com>
2026-06-06 13:07:05 +0700

    add node to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [preact()],
