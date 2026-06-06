09e9952b5bddc4f553cb6de55a43578ce1979d5d
phamlam24 <lamphamthedev@gmail.com>
2026-06-06 13:05:50 +0700

    remove vercel from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwindcss from '@tailwindcss/vite';
 
-import vercel from '@astrojs/vercel';
-
 export default defineConfig({
   integrations: [preact()],
   output: 'server',
